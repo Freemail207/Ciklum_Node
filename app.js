117ad32831105cc25b9046c20b7037caf968ff8e
Andrew Andriievskiy <andrewandriievskiy@MacBook-Pro-Andrew.local>
2019-10-10 12:57:07 +0300

    Add sigterm and sigint

--- app.js
+++ app.js
@@ -5,19 +5,30 @@ let KoaBodyparser = require('koa-bodyparser');
 let app = new Koa();
 let router = new Router();
 //TODO sigterm
-//TODO bodyparser
 app.use(KoaBodyparser());
 router
     .get('/', (ctx, next) => {
-        console.log(ctx)
         ctx.body = 'Hello World';
     })
     .post('/users', async (ctx, next) => {
         ctx.body = ctx.request.body;
     })
-
 app
     .use(router.routes())
     .use(router.allowedMethods());
+
+
+process.stdin.resume();
+
+process.on('SIGINT', () => {
+    console.log('Received SIGINT. Press Control-Z to exit.');
+});
+
+function handle(signal) {
+    console.log(`Received ${signal}`);
+}
+
+process.on('SIGINT', handle);
+process.on('SIGTERM', handle);
 
 app.listen(3000);
